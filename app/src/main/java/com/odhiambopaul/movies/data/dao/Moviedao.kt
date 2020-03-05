e20674f5a71b476d7649997b3038408c14bd9dc1
paulodhiambo <44492906+paulodhiambo@users.noreply.github.com>
2020-03-05 18:40:05 +0300

    added explanations

--- app/src/main/java/com/odhiambopaul/movies/data/dao/Moviedao.kt
+++ app/src/main/java/com/odhiambopaul/movies/data/dao/Moviedao.kt
@@ -12,10 +12,20 @@ import io.reactivex.Flowable
 interface Moviedao {
     //function to insert movies to the local database
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveMovie(movie:MovieEntity):Completable
+    fun saveMovie(movie: MovieEntity): Completable
+
+    /**
+     * Completable concerned with execution completion whether the task has reach to completion or some error has occurred.*/
 
     //function to retrieve all the movies from the local database
     @Query("SELECT * FROM Movie")
-    fun getAllMovies():Flowable<List<MovieEntity>>
+    fun getAllMovies(): Flowable<List<MovieEntity>>
+    /**
+    If there is not any data in your database and the query return no row the Flowable will not emit, neither onNext,
+    nor onError.
+    “When there is already an entry of ModelClass object in your database, Flowable will trigger onNext.
+    “Now if again you add new data or update your previous inserted object then the Flowable object will emit automatically,
+    allowing you to update the UI based on the latest data.
+     */
 
 }
--- app/src/main/java/com/odhiambopaul/movies/ui/detail/Moviedetail.kt
+++ app/src/main/java/com/odhiambopaul/movies/ui/detail/Moviedetail.kt
@@ -1,12 +1,11 @@
 package com.odhiambopaul.movies.ui.detail
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.odhiambopaul.movies.R
@@ -20,38 +19,35 @@ import kotlinx.android.synthetic.main.activity_moviedetail.*
 
 class Moviedetail : AppCompatActivity() {
 
-    private val compositeDisposable =CompositeDisposable()
+    private val compositeDisposable = CompositeDisposable()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_moviedetail)
-        val poster_text:String?=intent.getStringExtra("poster")
-        val title_text:String?=intent.getStringExtra("title")
-        val overview_text:String?=intent.getStringExtra("overview")
-        val release_text:String?=intent.getStringExtra("release")
-        val ratingtext:String?=intent.getStringExtra("rating")
+        val poster_text: String? = intent.getStringExtra("poster")
+        val title_text: String? = intent.getStringExtra("title")
+        val overview_text: String? = intent.getStringExtra("overview")
+        val release_text: String? = intent.getStringExtra("release")
+        val ratingtext: String? = intent.getStringExtra("rating")
 
         val photo: ImageView = findViewById(R.id.moviephoto)
         val title: TextView = findViewById(R.id.movietitle)
         val overview: TextView = findViewById(R.id.movieoverview)
         val rating: TextView = findViewById(R.id.movierating)
-        val release:TextView = findViewById(R.id.movierelease)
+        val release: TextView = findViewById(R.id.movierelease)
 
 
         Glide.with(this).load(poster_text).into(photo)
-        title.text= "Title: $title_text"
-        overview.text= "Overview: $overview_text"
-        rating.text= "Rating: $ratingtext"
-        release.text="Release date: $release_text"
-        if (ratingtext.isNullOrEmpty())
-        {
-            rating.text= "Rating: 1"
-        }
-        else
-        {
+        title.text = "Title: $title_text"
+        overview.text = "Overview: $overview_text"
+        rating.text = "Rating: $ratingtext"
+        release.text = "Release date: $release_text"
+        if (ratingtext.isNullOrEmpty()) {
+            rating.text = "Rating: 1"
+        } else {
 
-            rating.text= "Rating: $ratingtext"
+            rating.text = "Rating: $ratingtext"
         }
 
         compositeDisposable.add(
@@ -61,15 +57,17 @@ class Moviedetail : AppCompatActivity() {
                 .subscribe({ response -> onResponse(response) }, { t -> onFailure(t) })
         )
     }
+
     private fun onFailure(t: Throwable?) {
-        Toast.makeText(this,t?.message,Toast.LENGTH_LONG).show()
+        Toast.makeText(this, t?.message, Toast.LENGTH_LONG).show()
     }
 
     private fun onResponse(response: Response?) {
         top_rated_recycler.apply {
             setHasFixedSize(true)
-            layoutManager=LinearLayoutManager(this@Moviedetail,LinearLayoutManager.HORIZONTAL,false)
-            adapter=TopRatedAdapter(response?.results!!,this@Moviedetail)
+            layoutManager =
+                LinearLayoutManager(this@Moviedetail, LinearLayoutManager.HORIZONTAL, false)
+            adapter = TopRatedAdapter(response?.results!!, this@Moviedetail)
         }
     }
 }
