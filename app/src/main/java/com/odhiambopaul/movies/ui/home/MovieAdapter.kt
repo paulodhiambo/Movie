c5c7f20b6e5b90ceb8ca4d210d17398834aefb4b
paulodhiambo <44492906+paulodhiambo@users.noreply.github.com>
2020-03-05 18:44:08 +0300

    project update

--- app/src/main/java/com/odhiambopaul/movies/ui/home/MovieAdapter.kt
+++ app/src/main/java/com/odhiambopaul/movies/ui/home/MovieAdapter.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.odhiambopaul.movies.R
--- app/src/main/java/com/odhiambopaul/movies/utils/Constants.kt
+++ app/src/main/java/com/odhiambopaul/movies/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.odhiambopaul.movies.utils
 
-const val BASE_URL="https://api.themoviedb.org/"
-const val api_key="cfacbd1b17a84295a04a55d573daa740"
-const val image_path= "https://image.tmdb.org/t/p/w500/"
+const val BASE_URL = "https://api.themoviedb.org/"
+const val api_key = "cfacbd1b17a84295a04a55d573daa740"
+const val image_path = "https://image.tmdb.org/t/p/w500/"
